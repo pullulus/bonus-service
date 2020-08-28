8a30e0f8c2b6e53d642567fc7310db52d39d1234
Elena Tsypileva <etsypileva@gmail.com>
2020-08-28 13:48:44 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,8 @@ public class Main {
   public static void main(String[] args) {
     BonusService service = new BonusService();
 
-    long amount = 1000_60;
-    boolean registered = true;
-
-    service.calculate(amount, registered);
+    long bonusBelowlimitForRegistered = service.calculate(1000_60, true);
+    System.out.println(bonusBelowlimitForRegistered);
   }
 }
 
